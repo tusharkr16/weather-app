8d4a1a77767a4a54ae57cc38938c741e2430407b
tusharkr16 <tusharkumar9871@gmail.com>
2021-05-22 00:43:23 +0530

    changes the loop, made variable for icons,made code short and clean

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
+
+const localTimestring = (time) => {
+    const date = new Date(time * 1000)
+    return date.toLocaleTimeString();
+}
+
 function getWeather(event) {
     event.preventDefault();
     const city = document.getElementById('cityweather').value;
     console.log(city);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c194c45c83e3d89b18eae23afe5a48c6&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.WEATHER_API_KEY}&units=metric`)
         .then((apidata) => {
             console.log(apidata)
             return apidata.json();
         })
         .then((data) => {
             const sunrise = data.sys.sunrise;
-            const sunset= data.sys.sunset;
+            const sunset = data.sys.sunset;
             const temp = data.main.temp;
-            const date = new Date(sunrise* 1000);
-            const timestr = date.toLocaleTimeString();
+            const sunriseTime = localTimestring(sunrise);
+            const sunsetTime = localTimestring(sunset);
             const humidity = data.main.humidity;
-            const minTemp= data.main.temp_min;
-            const maxTemp=data.main.temp_max;
-           
+            const minTemp = data.main.temp_min;
+            const maxTemp = data.main.temp_max;
+            const sunIcon = '<i class="fas fa-sun"></i>';
+            const snowIcon = '<i class="fas fa-snowflake"></i>';
+            const cloudIcon = '<i class="fas fa-sun"></i>';
+            let icon = cloudIcon; 
 
-            document.getElementById('print').innerHTML = `temperature= ${temp} °C  <br> sunrise =${timestr} <br> sunset=${sunset} <br> Max Temperature=${maxTemp} <br> Min Temperature=${minTemp} <br> Humidity=${humidity} <br>`;
-            console.log(data)
+                     if (temp >= 25 && temp <= 60) {
+                             icon = sunIcon;
+                           
 
-            
-            if(temp<=15 && temp>30){
-              
-                document.body.style.backgroundColor="linear-gradient(to bottom, #ffffff, #fffc00)";
-                document.getElementById('print').innerHTML= `Temperature = ${temp} °C <i class="fas fa-sun"></i> <br> sunrise =${timestr} <br> sunset=${sunset} <br> Max Temperature=${maxTemp} <br> Min Temperature=${minTemp} <br> Humidity=${humidity} `;
-                
-            }
+                         }
 
-            else if(temp>14 && temp<0){
-                document.body.style.backgroundColor=" linear-gradient(to right, #005aa7, #fffde4)";
-                document.getElementById('print').innerHTML= `Temperature is ${temp} °C <i class"<i class="fas fa-cloud-sun"></i>"  <br> sunrise ${timestr}  <br> sunset=${sunset} <br> Max Temperature=${maxTemp} <br> Min Temperature=${minTemp} <br> Humidity=${humidity} <br> `;
-            }
-            else{
-                document.body.style.backgroundColor="linear-gradient(to right, #3a7bd5, #00d2ff)";
-                document.getElementById('print').innerHTML= `Temperature is ${temp} °C <i class"<i class="fas fa-snowflake"></i>"  <br> sunrise ${timestr} <br> sunset=${sunset} <br> Max Temperature=${maxTemp} <br> Min Temperature=${minTemp} <br> Humidity=${humidity} <br>`;
-               
-            }
-        
+                         else if (temp <= 0 && temp >= -20) {
+                             icon = snowIcon;
+                        }
+
+
+
+            document.getElementById('print').innerHTML = `Temperature = ${temp} °C ${icon} <br> sunrise ${localTimestring()}  <br> sunset=${localTimestring()} <br> Max Temperature=${maxTemp} <br> Min Temperature=${minTemp} <br> Humidity=${humidity} <br> `;
+
+
+
+
+
         })
         .catch((error) => {
 
